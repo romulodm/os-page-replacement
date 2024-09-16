0922687f8b1a40292b806403c51ea7d209fcfe41
Romulo <romulotg12@gmail.com>
2024-09-16 01:05:41 -0300

    fix: fixing the second chance, I was putting a new page in memory and setting the r (referenced) bit as true

--- go/algorithms/second_chance.go
+++ go/algorithms/second_chance.go
@@ -2,7 +2,7 @@ package algorithms
 
 // Page structure for the Second Chance algorithm
 type Page struct {
-	pageNumber string
+	pageID     string
 	referenced bool
 }
 
@@ -11,52 +11,51 @@ type Page struct {
 // If the reference bit is 1, the page is given a second chance and its bit is reset to 0;
 // If the bit is 0, the page is replaced. This algorithm aims to balance simplicity and efficiency;
 func SecondChance(accesses []string, numFrames int) (int, map[string]int) {
-	memory := make([]Page, numFrames)  // Initialize memory with a fixed size, tracking the pages and their reference bits
-	pageMap := make(map[string]int)    // Map to track page positions in memory
-	queue := make([]int, 0, numFrames) // Queue to manage page order
+	// Initialize memory and related structures
+	memory := make([]Page, numFrames) // Memory frames (numFrames)
+	pageMap := make(map[string]int)   // To track if a page is in memory
 
-	faults := 0
-	loads := make(map[string]int) // Counter to count the loads of each page
+	faults := 0                   // Page fault counter
+	loads := make(map[string]int) // To track how many times each page is loaded
 
-	pos := 0 // Pointer for the page to be replaced
+	pos := 0 // The pointer for replacement
 
 	for _, page := range accesses {
 		// Check if the page is already in memory
 		if index, found := pageMap[page]; found {
-			// If the page is already in memory, give it a second chance by setting the reference bit to true
+			// Page is found in memory, update reference bit to true (second chance)
 			memory[index].referenced = true
 			continue
 		}
 
-		// Page fault occurs if the page is not in memory
+		// Page fault occurs (page not found in memory)
 		faults++
-		loads[page]++ // Increment the page counter
+		loads[page]++ // Count how many times the page has been loaded
 
-		// If memory is full, we need to replace a page
-		if len(queue) >= numFrames {
-			for {
-				// Check the reference bit of the page at the pointer
-				if !memory[queue[pos]].referenced {
-					// If the reference bit is 0, replace this page
-					pageToRemove := memory[queue[pos]].pageNumber
-					delete(pageMap, pageToRemove)
-					memory[queue[pos]] = Page{pageNumber: page, referenced: true}
-					pageMap[page] = queue[pos]
-					queue[pos] = (queue[pos] + 1) % numFrames // Move the pointer forward
-					break
-				} else {
-					// If the reference bit is 1, reset it to 0 and give the page a second chance
-					memory[queue[pos]].referenced = false
-					queue[pos] = (queue[pos] + 1) % numFrames
-				}
+		// If memory is full, replace a page using the second chance algorithm
+		for {
+			// Check the reference bit at the current pointer position
+			if !memory[pos].referenced {
+				// Remove the old page from pageMap
+				oldPage := memory[pos].pageID
+				delete(pageMap, oldPage)
+
+				// Add the new page to memory
+				memory[pos] = Page{pageID: page, referenced: false}
+				pageMap[page] = pos // Track the position of the new page in memory
+
+				// Move pointer to the next position (circularly)
+				pos = (pos + 1) % numFrames
+				break
+			} else {
+				// If the reference bit is 1, reset it to 0 and give it a second chance
+				memory[pos].referenced = false
+				// Move pointer to the next position (circularly)
+				pos = (pos + 1) % numFrames
 			}
-		} else {
-			// If there is space in memory, simply add the new page
-			memory[len(queue)] = Page{pageNumber: page, referenced: true}
-			pageMap[page] = len(queue)
-			queue = append(queue, len(queue))
 		}
 	}
 
+	// Return the number of page faults and the loads of each page
 	return faults, loads
 }
